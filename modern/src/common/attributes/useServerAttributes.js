5ed2fc14c7ae56fda66bc013f45b8fe2fbec28ce
samned <samuelabebayehu@gmail.com>
2023-05-03 02:23:11 +0300

    update language

--- modern/src/common/attributes/useServerAttributes.js
+++
@@ -1,8 +0,0 @@
-import { useMemo } from 'react';
-
-export default (t) => useMemo(() => ({
-  'ui.disableLoginLanguage': {
-    name: t('attributeUiDisableLoginLanguage'),
-    type: 'boolean',
-  },
-}), [t]);
