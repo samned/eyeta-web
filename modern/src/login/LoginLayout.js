d6de81186e3fb74ec511bbba6c37ac3e71d77114
samned <samuelabebayehu@gmail.com>
2023-12-25 08:31:10 +0300

    snow

--- modern/src/login/LoginLayout.js
+++ modern/src/login/LoginLayout.js
@@ -1,45 +1,46 @@
-import React from 'react';
-import { useMediaQuery, Paper } from '@mui/material';
-import { grey } from '@mui/material/colors';
-import makeStyles from '@mui/styles/makeStyles';
-import { useTheme } from '@mui/material/styles';
-import LogoImage from './LogoImage';
+import React from "react";
+import { useMediaQuery, Paper } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import makeStyles from "@mui/styles/makeStyles";
+import { useTheme } from "@mui/material/styles";
+import Snowfall from "react-snowfall";
+import LogoImage from "./LogoImage";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    height: '100%',
+    display: "flex",
+    height: "100%",
   },
   sidebar: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     background: theme.palette.primary.main,
     paddingBottom: theme.spacing(5),
     width: theme.dimensions.sidebarWidth,
-    [theme.breakpoints.down('lg')]: {
+    [theme.breakpoints.down("lg")]: {
       width: theme.dimensions.sidebarWidthTablet,
     },
-    [theme.breakpoints.down('sm')]: {
-      width: '0px',
+    [theme.breakpoints.down("sm")]: {
+      width: "0px",
     },
   },
   paper: {
     background: grey[200],
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
-    boxShadow: '-2px 0px 16px rgba(0, 0, 0, 0.25)',
-    [theme.breakpoints.up('lg')]: {
+    boxShadow: "-2px 0px 16px rgba(0, 0, 0, 0.25)",
+    [theme.breakpoints.up("lg")]: {
       padding: theme.spacing(0, 25, 0, 0),
     },
   },
   form: {
     maxWidth: theme.spacing(52),
     padding: theme.spacing(5),
-    width: '100%',
+    width: "100%",
   },
 }));
 
@@ -49,13 +50,14 @@ const LoginLayout = ({ children }) => {
 
   return (
     <main className={classes.root}>
+      <Snowfall />
       <div className={classes.sidebar}>
-        {!useMediaQuery(theme.breakpoints.down('lg')) && <LogoImage color={theme.palette.secondary.contrastText} />}
+        {!useMediaQuery(theme.breakpoints.down("lg")) && (
+          <LogoImage color={theme.palette.secondary.contrastText} />
+        )}
       </div>
       <Paper className={classes.paper}>
-        <form className={classes.form}>
-          {children}
-        </form>
+        <form className={classes.form}>{children}</form>
       </Paper>
     </main>
   );
